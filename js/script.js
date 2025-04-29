66f034bdf62b02bfcb333af7d073e330f0bce520
v.chaulytka <vvictorims123@gmail.com>
2025-04-29 10:52:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const arr = ['a', 'b', 'c'];
+const arr1 = {
+    a: 'a',
+    1: 'b',
+    2: 'c'
+}
+
+arr1.b = '1234';
+
+
+console.log(arr1.b);
+
+
+const obj = {
+    'Anna': 500,
+    'Alice': 800
+};
